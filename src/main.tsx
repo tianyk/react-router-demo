05aecd26fa3834748dc44934cdd7bdd27a1d5807
yongketian@gmail.com <tyk>
2023-04-04 18:53:01 +0800

    context

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Link } from 'react-router-dom';
+import { useInRouterContext, Link } from 'react-router-dom';
 
 function Href() {
+  const link = useInRouterContext() ? <Link to={"/console/users"}>列表2</Link> : <a href={"/console/users"}>列表2</a>;
   return <div>
     <a href="/console/users">列表</a>
-    {/* <Link to={"/console/users"}>列表2</Link> */}
+    {link}
   </div>
 }
 
